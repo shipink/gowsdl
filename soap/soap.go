c960830abaffbba4ee361b3dcf7214ca3bc5d7d1
Oytun Yuksel <oytun@tosih.com>
2022-07-28 11:50:48 +0300

    feat: add namespace support to response

--- soap/soap.go
+++ soap/soap.go
@@ -22,7 +22,8 @@ type SOAPDecoder interface {
 }
 
 type SOAPEnvelopeResponse struct {
-	XMLName     xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	XMLName     xml.Name `xml:"soap:Envelope"`
+	XmlNS       string   `xml:"xmlns:soap,attr"`
 	Header      *SOAPHeaderResponse
 	Body        SOAPBodyResponse
 	Attachments []MIMEMultipartAttachment `xml:"attachments,omitempty"`
@@ -514,7 +515,12 @@ func (s *Client) call(ctx context.Context, soapAction string, request, response 
 
 	// xml Decoder (used with and without MTOM) cannot handle namespace prefixes (yet),
 	// so we have to use a namespace-less response envelope
-	respEnvelope := new(SOAPEnvelopeResponse)
+	respEnvelope := SOAPEnvelopeResponse{XmlNS: XmlNsSoapEnv}
+
+	if s.opts.customEnvelope != "" {
+		respEnvelope.XmlNS = s.opts.customEnvelope
+	}
+
 	respEnvelope.Body = SOAPBodyResponse{
 		Content: response,
 		Fault: &SOAPFault{
